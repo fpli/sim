a775749619bfc8afd679d5f6f421ecd62f8ad333
fpli <1394220859@qq.com>
2019-08-13 09:08:20 +0800

    添加依赖包

--- app/src/main/java/com/sdust/im/action/UserAction.java
+++ app/src/main/java/com/sdust/im/action/UserAction.java
@@ -8,6 +8,9 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ *
+ */
 public class UserAction {
 
 	private static NetService mNetService = NetService.getInstance();
